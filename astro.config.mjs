b4b1745d7d4ec4745b60ac6325df8ad0247ac79d
space-otter <space-otter@visualhackstudio.com>
2024-05-08 19:32:48 +0200

    Add netlify and solidjs adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [solidJs()]
+});
